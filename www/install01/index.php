9481d834e8071189d55c70cb8735605e6daf0b60
Pgiraudeau85 <pgiraudeau85@gmail.com>
2013-12-12 08:39:56 +0100

    manual update
    test de modification à la main

--- www/install01/index.php
+++ www/install01/index.php
@@ -6,9 +6,47 @@
         <script type="text/javascript" src="../_/js/bootstrap.min.js"></script>
     </head>
     <body>
+        <nav class="navbar navbar-inverse" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"> <b> <h4 align="center">AcaJuegar - Cuestionario sobre América Latina</h4>  </b></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+
+
+                    <li class="dropdown">
+
+
+                    </li>
+                </ul>
+                <form class="navbar-form navbar-left" role="search">
+                    <div class="form-group">
+
+                    </div>
+
+                </form>
+                <ul class="nav navbar-nav navbar-right">
+
+                    <li class="dropdown">
+
+
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </nav>
+
         <?php
         include('c:/wamp/www/AcaJuegar/installs/install01/include.php');
-        include(ROOT.'index.php');
+        include(ROOT . 'index.php');
         ?>
     </body>
 </html>
