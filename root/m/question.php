f331c892969d7d78d16adbf49f90471a0fc7cfc1
matthieurobin <robin10mat@gmail.com>
2014-01-09 13:24:11 +0100

    switch mysql to postgresql

--- root/m/question.php
+++ root/m/question.php
@@ -5,8 +5,8 @@ class ROOT_M_Question extends MVC_Table {
     protected $_tableRow = 'ROOT_M_QuestionRow';
     
     public static function getRandomQuestions(){
-        //return ROOT_M_Question::getInstance()->select('SELECT * FROM question ORDER BY RANDOM() LIMIT 20',array());
-        return ROOT_M_Question::getInstance()->select('SELECT * FROM question ORDER BY RAND() LIMIT 20',array());
+        return ROOT_M_Question::getInstance()->select('SELECT * FROM question ORDER BY RANDOM() LIMIT 20',array());
+        //return ROOT_M_Question::getInstance()->select('SELECT * FROM question ORDER BY RAND() LIMIT 20',array());
     }
     
     public static function getQuestion($id){
